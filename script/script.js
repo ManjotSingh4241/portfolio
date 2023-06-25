bbfe3fd9a5e01956eadb7a54cf517f9fba833d00
Manjot singh <82448988+ManjotSingh4241@users.noreply.github.com>
2023-06-24 23:18:31 -0400

    modified css

--- script/script.js
+++ script/script.js
@@ -1,30 +1,31 @@
 //////////////////////////////////////////////////////////////////////////////////////////////Scroll///////////////
 // Function to check if an element is in the viewport
-// function isInViewport(element) {
-//     var rect = element.getBoundingClientRect();
-//     return (
-//       rect.top >= 0 &&
-//       rect.left >= 0 &&
-//       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-//       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//     );
-//   }
-  
-//   // Function to handle the scroll event
-//   function handleScroll() {
-//     var elements = document.querySelectorAll('#pro, #pro3, #pro2, #pro4');
-  
-//     for (var i = 0; i < elements.length; i++) {
-//       var element = elements[i];
-//       if (isInViewport(element)) {
-//         element.classList.add('is-visible');
-//       } else {
-//         element.classList.remove('is-visible');
-//       }
-//     }
-//   }
+function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Function to handle the scroll event
+function handleScroll() {
+    var elements = document.querySelectorAll('#pro, #pro3, #pro2, #pro4, .bottom');
+
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        if (isInViewport(element)) {
+            element.classList.add('is-visible');
+        } else {
+            element.classList.remove('is-visible');
+        }
+    }
+}
 
 
-//   // Add scroll event listener
-//   window.addEventListener('scroll', handleScroll);
+// Add scroll event listener
+window.addEventListener('scroll', handleScroll);
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
